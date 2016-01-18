f45389514fc296488aeef64b9107d9a838eb9f98
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2016-01-18 11:21:05 +0900

    detect test file after Ruby 2.4, it's obsoleted

--- chkbuild/ruby.rb
+++ chkbuild/ruby.rb
@@ -484,7 +484,8 @@ def (ChkBuild::Ruby).build_proc(b)
       b.catch_error { b.make("btest", "OPTS=-v -q", make_options.merge(:section=>"btest")) }
     end
     b.catch_error {
-      b.run("./miniruby", "#{srcdir+'sample/test.rb'}", :section=>"test.rb")
+      test_file = File.exist?(srcdir+'basictest/test.rb') ? 'basictest/test.rb' : 'sample/test.rb'
+      b.run("./miniruby", "#{srcdir+test_file}", :section=>"test.rb")
       if /^end of test/ !~ b.logfile.get_section('test.rb')
         raise ChkBuild::Build::CommandError.new(0, "test.rb")
       end
